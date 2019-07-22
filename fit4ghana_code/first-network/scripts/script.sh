f6204890aa31ef631aa2bb9beafd118f4d6b623f
Neha Gopinath <ga62moc@mytum.de>
2019-07-22 17:58:37 +0200

    Changes to network

--- fit4ghana_code/first-network/scripts/script.sh
+++ fit4ghana_code/first-network/scripts/script.sh
@@ -86,7 +86,7 @@ installChaincode 0 2
 
 # Instantiate chaincode on peer0.org2
 #echo "Instantiating chaincode on peer0.org2..."
-#instantiateChaincode 0 2
+instantiateChaincode 0 2
 
 # Query chaincode on peer0.org1
 #echo "Querying chaincode on peer0.org1..."
@@ -95,6 +95,8 @@ installChaincode 0 2
 # Invoke chaincode on peer0.org1 and peer0.org2
 #echo "Sending invoke transaction on peer0.org1 peer0.org2..."
 #chaincodeInvoke 0 1 0 2
+
+#chaincodeInvoke 1 1 1 2
 
 ## Install chaincode on peer1.org2
 echo "Installing chaincode on peer1.org2..."
