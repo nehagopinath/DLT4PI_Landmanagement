5723b86b1077749c5d01e7d5c2f9706fca2aaa7c
Sukanya Dasgupta <ga53sid@mytum.de>
2019-07-23 22:54:43 +0200

    family-member service

---
+++ fit4ghana-angular/src/services/family-member.service.ts
@@ -0,0 +1,13 @@
+import { FamilyMember } from 'src/models/family-member';
+
+export class FamilyMemberService {
+    familyMembers: FamilyMember;
+
+    getAllFamilyMembers(): FamilyMember[] {
+        return [];
+    }
+
+    getFamilyMemberByID(): FamilyMember {
+        return null;
+    }
+}
