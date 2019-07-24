9fa1cd72f5a6c3de13fb41bbb603879d60ad7ae4
Arnd Pettirsch <arndpettirsch@Arnds-MacBook-Pro.local>
2019-07-24 23:54:37 +0200

    Small fixes

--- fit4ghana-node-server/nodejs-server/src/routes/registerLand.js
+++ fit4ghana-node-server/nodejs-server/src/routes/registerLand.js
@@ -8,15 +8,15 @@ const RequestController = require('../controllers/requestController')
 const LandController = require('../controllers/landController')
 
 //e.g.: create/familymember/statutory/chief/cls
-router.post('create/:user/:landtype/:chief/:approver', LandRegisterController.create);
+router.post('/create/:user/:landtype/:chief/:approver', LandRegisterController.create);
 
 router.get('/getAllRegistrationRequestAwating/:approver', RequestController.query);
 router.get('/getAllSellBuyRequestAwating/:approver', RequestController.queryBuySell);
 
 router.get('/queryLand/:user', LandController.query);
 router.get('/queryLandForSale/:user', LandController.queryForSale);
-router.post('putForSale/:landnumber', LandController.putForSale);
-router.post('withDrawLandFromSale/:landnumber', LandController.withdrawFromSale);
+router.post('/putForSale/:landnumber', LandController.putForSale);
+router.post('/withDrawLandFromSale/:landnumber', LandController.withdrawFromSale);
 router.post('/requestLandRegistration/:landnumber/:registrationType/:user', LandController.register);
 router.post('requestLandTransaction/:landnumber/:seller/:buyer/:price', LandController.transact);
 
